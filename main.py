f4810225df3ed87ab56fcafdc726eba7fb25b19b
ismaeelhaider12 <90700806+ismaeelhaider12@users.noreply.github.com>
2021-11-01 12:36:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,11 @@ if flag:
 if not flag:
     print(num, "is a prime number")
 
+def add_underscores(word):
+    new_word = "_"
+    for i in range(len(word)):
+        new_word = word[i] + "_"
+    return new_word
+
+phrase = "hello"
+print(add_underscores(phrase))
